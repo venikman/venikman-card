4f78d71ce818861addb648fb3b5af1b92f4a9bb2
Stan <nedbailov375426@gmail.com>
2018-12-11 12:39:12 -0500

    Replace using ink to enquier.

--- cli.js
+++ cli.js
@@ -1,14 +1,38 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
-const importJsx = require('import-jsx');
-const { h, render } = require('ink');
-
-const ui = importJsx('./ui.jsx');
+const chalk = require('chalk');
+const enquirer = require('enquirer');
+const opn = require('opn');
 
 meow(`
 	Usage
 	  $ venikman
 `);
 
-render(h(ui));
+const start = async () => {
+    const chooseLinks = {
+        type    : 'select',
+        name    : 'answer',
+        message : chalk.green('Hi! I am venikman.'),
+        choices : [...['Here is my website', 'Here is my twitter', 'Here is my github'].map((choise) => {
+            return chalk.red(choise);
+        }), 'Quit']
+    };
+    const answerChooseLinks = await enquirer.prompt(chooseLinks);
+    return answerChooseLinks;
+};
+
+// eslint-disable-next-line promise/prefer-await-to-then
+start().then((res) => {
+    if (res.answer.includes('Here is my twitter')) {
+        opn('https://twitter.com/venik_man');
+    }
+    if (res.answer.includes('Here is my github')) {
+        opn('https://github.com/venikman');
+    }
+    if (res.answer.includes('Here is my website')) {
+        opn('https://nedbailov.com');
+    }
+    process.exit();
+});
