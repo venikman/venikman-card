b3b9bf1f2a3168c54c0c710b81abf78ec16585c5
Stan <nedbailov375426@gmail.com>
2019-01-22 12:32:43 -0500

    Add feature to send me sms and run stdlib func

--- cli.js
+++ cli.js
@@ -4,18 +4,26 @@ const meow = require('meow');
 const chalk = require('chalk');
 const enquirer = require('enquirer');
 const opn = require('opn');
+const Lib = require('lib')
+const envy = require('envy');
 
 meow(`
-	Usage
-	  $ venikman
+Usage
+$ venikman
 `);
+
+const env = envy();
+const lib = Lib({ token : env.token });
+const sms = lib.utils.sms['@1.0.9'];
+const { phoneNumber } = env;
+
 
 const start = async () => {
     const chooseLinks = {
         type    : 'select',
         name    : 'answer',
         message : chalk.green('Hi! I am venikman.'),
-        choices : [...['Here is my website', 'Here is my twitter', 'Here is my github'].map((choise) => {
+        choices : [...['Here is my website', 'Here is my twitter', 'Here is my github', 'Use stdlib FAAS', 'Send me SMS'].map((choise) => {
             return chalk.red(choise);
         }), 'Quit']
     };
@@ -24,15 +32,43 @@ const start = async () => {
 };
 
 // eslint-disable-next-line promise/prefer-await-to-then
-start().then((res) => {
-    if (res.answer.includes('Here is my twitter')) {
+start().then(async (res) => {
+    if (res.answer.includes('twitter')) {
         opn('https://twitter.com/venik_man');
     }
-    if (res.answer.includes('Here is my github')) {
+    if (res.answer.includes('github')) {
         opn('https://github.com/venikman');
     }
-    if (res.answer.includes('Here is my website')) {
+    if (res.answer.includes('website')) {
         opn('https://nedbailov.com');
+    }
+    if (res.answer.includes('FAAS')) {
+        const stdlib = await lib.venikman.sms['@dev']({ name : 'testName' });
+        console.log(stdlib);
+    }
+    if (res.answer.includes('SMS')) {
+        const smsForm = {
+            name: 'form',
+            type: 'form',
+            message: 'Please provide your information:',
+            choices: [
+                { name: 'name', message: 'Yout name', initial: 'name...' },
+                { name: 'message', message: 'Sms text', initial: 'tell me something' }
+            ]
+        };
+        const answer = await enquirer.prompt(smsForm);
+        const { name, message } =answer.form;
+
+        try {
+            await sms({
+                to   : phoneNumber,
+                body : `From ${name}. ${message}.`
+            });
+            console.log(`hello ${name}, I am venikman. I got your message.`);
+        }
+        catch (error) {
+            console.log('We got problem to send a Message.');
+        }
     }
     process.exit();
 });
